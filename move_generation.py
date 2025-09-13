40059a72f863aebc142d41d26395e40e2de408b5
Radha Krishna <pradhakrishna594@gmail.com>
2025-09-14 01:13:00 +0530

    All Black piece moves added

--- move_generation.py
+++ move_generation.py
@@ -130,4 +130,111 @@ def generate_white_king_moves(board):
                             moves.append(((r, c), (nr, nc)))
     return moves
 
+def generate_black_pawn_moves(board):
+    moves = []
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 'p':
+                if r < 7 and board[r+1][c] == '.':
+                    moves.append(((r, c), (r+1, c)))
+                    if r == 1 and board[r+2][c] == '.':
+                        moves.append(((r, c), (r+2, c)))
+                if r < 7 and c > 0 and board[r+1][c-1].isupper():
+                    moves.append(((r, c), (r+1, c-1)))
+                if r < 7 and c < 7 and board[r+1][c+1].isupper():
+                    moves.append(((r, c), (r+1, c+1)))
+    return moves
 
+def generate_black_knight_moves(board):
+    moves = []
+    knight_moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2),
+                    (1, -2), (1, 2), (2, -1), (2, 1)]
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 'n':
+                for dr, dc in knight_moves:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < 8 and 0 <= nc < 8:
+                        target = board[nr][nc]
+                        if target == '.' or target.isupper():
+                            moves.append(((r, c), (nr, nc)))
+    return moves
+
+def generate_black_bishop_moves(board):
+    moves = []
+    directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 'b':
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    while 0 <= nr < 8 and 0 <= nc < 8:
+                        target = board[nr][nc]
+                        if target == '.':
+                            moves.append(((r, c), (nr, nc)))
+                        elif target.isupper():
+                            moves.append(((r, c), (nr, nc)))
+                            break
+                        else:
+                            break
+                        nr += dr
+                        nc += dc
+    return moves
+
+def generate_black_rook_moves(board):
+    moves = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 'r':
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    while 0 <= nr < 8 and 0 <= nc < 8:
+                        target = board[nr][nc]
+                        if target == '.':
+                            moves.append(((r, c), (nr, nc)))
+                        elif target.isupper():
+                            moves.append(((r, c), (nr, nc)))
+                            break
+                        else:
+                            break
+                        nr += dr
+                        nc += dc
+    return moves
+
+def generate_black_queen_moves(board):
+    moves = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1),
+                  (-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 'q':
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    while 0 <= nr < 8 and 0 <= nc < 8:
+                        target = board[nr][nc]
+                        if target == '.':
+                            moves.append(((r, c), (nr, nc)))
+                        elif target.isupper():
+                            moves.append(((r, c), (nr, nc)))
+                            break
+                        else:
+                            break
+                        nr += dr
+                        nc += dc
+    return moves
+
+def generate_black_king_moves(board):
+    moves = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1),
+                  (-1, -1), (-1, 1), (1, -1), (1, 1)]
+    for r in range(8):
+        for c in range(8):
+            if board[r][c] == 'k':
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < 8 and 0 <= nc < 8:
+                        target = board[nr][nc]
+                        if target == '.' or target.isupper():
+                            moves.append(((r, c), (nr, nc)))
+    return moves
